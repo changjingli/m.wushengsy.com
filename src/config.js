6058edcb95d841d12865a8e964315f7aae88e5c8
Chang, Jingli <jingli_chang@foxmail.com>
2018-03-27 10:54:34 +0800

    增加公共配置文件

--- src/config.js
+++ src/config.js
@@ -1,3 +1,6 @@
 export default {
-	title: '欢迎光临上海雾胜实业有限公司'
+	title: '欢迎光临上海雾胜实业有限公司',
+	qq: 'http://wpa.qq.com/msgrd?v=3&uin=574916852&site=qq&menu=yes',
+	imgPrefix: 'http://www.wushengsy.com',
+	limit: 12,
 }
