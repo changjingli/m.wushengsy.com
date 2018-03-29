c9b1badf5446df385025d51a638851f864d8cf9d
Chang, Jingli <jingli_chang@foxmail.com>
2018-03-29 18:13:21 +0800

    2018年3月29日最后提交

--- src/app.vue
+++ src/app.vue
@@ -14,46 +14,74 @@
 				<h1 class="mui-title">{{ title }}</h1>
 			</header>
 
-			<div class="mui-content">
-				<!-- 主界面具体展示内容 -->
+			<div class="mui-content mui-scroll-wrapper">
+				<div class="mui-scroll">
+					<!-- 主界面具体展示内容 -->
 
-				<!-- 轮播开始 -->
-				<div class="mui-slider">
-					<div class="mui-slider-group">
-						<div class="mui-slider-item"><a href="#"><img src="./static/penwuchuchen.png" alt="喷雾除尘" /></a>
+					<!-- 轮播开始 -->
+					<div class="mui-slider">
+						<div class="mui-slider-group">
+							<div class="mui-slider-item"><a href="#"><img src="./static/penwuchuchen.png" alt="喷雾除尘" /></a>
+							</div>
+							<div class="mui-slider-item"><a href="#"><img src="./static/penwuchuchou.png" alt="喷雾除臭" /></a>
+							</div>
+							<div class="mui-slider-item"><a href="#"><img src="./static/penwujiangwen.png" alt="喷雾降温" /></a>
+							</div>
+							<div class="mui-slider-item"><a href="#"><img src="./static/penwujiashi.png"
+							                                              alt="喷雾加湿" /></a>
+							</div>
+							<div class="mui-slider-item"><a href="#"><img src="./static/jingguanzaowu.png" alt="景观造雾" /></a>
+							</div>
 						</div>
-						<div class="mui-slider-item"><a href="#"><img src="./static/penwuchuchou.png" alt="喷雾除臭" /></a>
-						</div>
-						<div class="mui-slider-item"><a href="#"><img src="./static/penwujiangwen.png" alt="喷雾降温" /></a>
-						</div>
-						<div class="mui-slider-item"><a href="#"><img src="./static/penwujiashi.png" alt="喷雾加湿" /></a>
-						</div>
-						<div class="mui-slider-item"><a href="#"><img src="./static/jingguanzaowu.png" alt="景观造雾" /></a>
+						<div class="mui-slider-indicator">
+							<div class="mui-indicator mui-active"></div>
+							<div class="mui-indicator"></div>
+							<div class="mui-indicator"></div>
+							<div class="mui-indicator"></div>
 						</div>
 					</div>
-					<div class="mui-slider-indicator">
-						<div class="mui-indicator mui-active"></div>
-						<div class="mui-indicator"></div>
-						<div class="mui-indicator"></div>
-						<div class="mui-indicator"></div>
+					<!-- 轮播结束 -->
+
+					<div class="mui-card">
+						<div class="mui-card-header">实用案例</div>
+						<div class="mui-card-content">
+							<ul class="mui-table-view mui-grid-view">
+								<li class="mui-table-view-cell mui-media mui-col-xs-6" v-for="(p, idx) in penwujiangwen"
+								    :key="idx">
+									<img class="mui-media-object" :src="p.thumb | addPrefix" :alt="p.title">
+									<div class="mui-media-body">{{ p.title }}</div>
+								</li>
+							</ul>
+						</div>
 					</div>
+
+					<div class="mui-card">
+						<div class="mui-card-header">合作伙伴</div>
+						<div class="mui-card-content">
+							<div class="mui-slider">
+								<div class="mui-slider-group">
+									<template v-for="(par) in partner">
+										<div class="mui-slider-item">
+											<ul class="mui-table-view mui-grid-view mui-grid-9">
+												<li class="mui-table-view-cell mui-media mui-col-xs-4"
+												    v-for="(p, idx) in par" :key="idx">
+													<img class="mui-media-object" :src="p.imagesPath | addPrefix"
+													     :alt="p.title">
+													<div class="mui-media-body">{{ p.title }}</div>
+												</li>
+											</ul>
+										</div>
+									</template>
+								</div>
+								<div class="mui-slider-indicator">
+									<div class="mui-indicator mui-active"></div>
+									<div class="mui-indicator"></div>
+								</div>
+							</div>
+						</div>
+					</div>
+
 				</div>
-				<!-- 轮播结束 -->
-
-				<div class="mui-card">
-					<div class="mui-card-header">实用案例</div>
-					<div class="mui-card-content">
-						<ul class="mui-table-view mui-grid-view">
-							<li class="mui-table-view-cell mui-media mui-col-xs-6 wow"
-							    :class="[idx % 2 ? 'slideInRight' : 'slideInLeft']" v-for="(p, idx) in penwujiangwen"
-							    :key="idx">
-								<img class="mui-media-object" :src="p.thumb | addPrefix">
-								<div class="mui-media-body">{{ p.title }}</div>
-							</li>
-						</ul>
-					</div>
-				</div>
-
 			</div>
 			<div class="mui-off-canvas-backdrop"></div>
 		</div>
@@ -64,7 +92,6 @@
 	import navLeft from './components/nav-left.vue';
 
 	import mui from './libs/mui/3.7.2/js/mui.min';
-	// import wow from 'wowjs';
 
 	import wsConfig from './config.js';
 	import util from './util';
@@ -76,7 +103,9 @@
 				title: wsConfig.title,
 				imgPrefix: wsConfig.imgPrefix,
 				limit: wsConfig.limit,
+
 				penwujiangwen: [],
+				partner: [],
 			}
 		},
 		components: {
@@ -85,15 +114,39 @@
 		created: function () {
 			// 7 获取喷雾降温列表
 			util.request( 'apis/case/getPenWuJiangWenList.php', ( data ) => {
-				this.penwujiangwen = data.slice(0, this.limit);
+				this.penwujiangwen = data.slice( 0, this.limit );
+			} );
+			// 12 获取合作伙伴列表
+			util.request( 'apis/system/getPartnerList.php', ( data ) => {
+
+				const split = 5; // 6个
+				let index = 0;
+				let arr = [];
+				data.forEach( ( item, idx ) => {
+
+					if ( !$.isArray( arr[ index ] ) ) {
+						arr[ index ] = [];
+					}
+
+					arr[ index ].push( item );
+
+					index = idx > 0 && idx % split === 0 ? ++index : index;
+				} );
+
+				this.partner = arr;
+
 			} );
 		},
 		mounted: function () {
-			// https://github.com/matthieua/WOW
-			let wows = new WOW();
+			mui( '.mui-scroll-wrapper' ).scroll( {
+				deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+			} );
 
-			wows.init();
+			console.log( 'mui(\'mui-slider\')', mui( '.mui-slider' ) );
 
+			mui( '.mui-slider' ).each( ( idx, ele ) => {
+				mui( ele ).slider();
+			} );
 		},
 		filters: {
 			addPrefix: function ( src ) {
@@ -104,7 +157,8 @@
 </script>
 
 <style scoped>
-	.mui-off-canvas-wrap {
-		overflow: auto;
+	.mui-table-view.mui-grid-view .mui-table-view-cell .mui-media-object {
+		height: 82px;
+		object-fit: cover;
 	}
 </style>
